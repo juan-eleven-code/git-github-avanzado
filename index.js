1945c2af3c7f6fc8cd8c34ebc8ea7e9d0ab90bf4
Juan Macías <info@juanmacias.es>
2025-05-18 15:48:15 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log('Hola Mundo');
+
+/**
+ * Prefijos de ramas
+ * 
+ * feature/nombre-de-la-funcionalidad
+ * bugfix/nombre-del-arreglo
+ * release/lanzamiento-de-version
+ * 
+ */
+
+/*
+ * Prefijos de Commits
+ * feat: Una nueva característica para el usuario.
+ * (feature / funcionalidad)
+ * 
+ * fix: Arregla un bug que afecta al usuario. 
+ * (bugfix / arreglo de fallo) 
+ * 
+ * perf: Cambios que mejoran el rendimiento del sitio.
+ * (performance / rendimiento)
+ * 
+ * chore: Cambios que afectan a la configuración del proyecto como tal.
+ * (chore / faena)
+ * 
+ * build: Cambios en el sistema de build, tareas de despliegue o instalación.
+ * (build / "compilacion")
+ * 
+ * ci: Cambios en la integración continua.
+ * (ci / continuous integration / integracion continua)
+ * 
+ * docs: Cambios en la documentación.
+ * (docs / documentacion)
+ * 
+ * refactor: Refactorización del código como cambios de nombre de variables o funciones.
+ * (refactor / rehacer / mejorar o cambios de un código a otro mejor)
+ * 
+ * style: Cambios de formato, tabulaciones, espacios o puntos y coma, etc; no afectan al usuario.
+ * (style / estilos)
+ * 
+ * test: Añade tests o refactoriza uno existente.
+ * (test / pruebas de código)
+ */
